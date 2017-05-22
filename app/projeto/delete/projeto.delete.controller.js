9e51b47790e2bcc9a290fa631c655b0bb3fe5355
JuanesAdriano <juaneshtk50@gmail.com>
2017-05-21 22:47:21 -0300

    Criando Module e controler do projeto.delete

--- app/projeto/delete/projeto.delete.controller.js
+++ app/projeto/delete/projeto.delete.controller.js
@@ -1,3 +1,43 @@
 /**
  * Created by juane on 19/05/2017.
  */
+var projetoDeleteController = angular.module('projeto.delete.controller', []);
+
+projetoDeleteController.controller('projetoDeleteCtrl',
+    ['$scope', '$backstageDialog', '$backstageToast', '$state', 'projetoFactory',
+        function ($scope, $backstageDialog, $backstageToast, $state, projetoFactory) {
+            var params = [];
+
+
+            $scope.excluir = function (evento) {
+                angular.element(document).ready(function () {
+
+                    params.push("pk_projeto=" + $state.params.id);
+                    // alert($stateParams.id);
+                });
+                $backstageDialog.renderConfirmDialog(
+                    evento,
+                    'Tem certeza?',
+                    'Tem certeza que deseja excluir este projeto?',
+                    'Confirmar exclusão do projeto',
+                    'Claro!',
+                    'Não, foi um engano.',
+                    function () {
+
+                        projetoFactory.destroy(params).remove()
+                            .$promise.then(
+                            function (value) {
+                                return $backstageToast.come(value.extra.icone, value.message);
+                            },
+
+                            function (error) {
+                                console.log("ERRO: " + error);
+                            }
+                        );
+                    }, function () {
+
+                    })
+            };
+
+
+        }]);
--- app/projeto/delete/projeto.delete.module.js
+++ app/projeto/delete/projeto.delete.module.js
@@ -1,3 +1,4 @@
 /**
  * Created by juane on 19/05/2017.
  */
+angular.module('projeto.delete.module',['projeto.delete.controller']);
