3e73b57432a49efbce088ef3db16b89c0d4e2c6d
JuanesAdriano <juaneshtk50@gmail.com>
2017-05-21 21:48:34 -0300

    Criando Projeto Module e Projeto Routes

---
+++ app/projeto/projeto.module.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Juanes on 21/05/2017.
+ */
+var projetoModule = angular.module('projeto.module' [
+        "projeto.login.module",
+        "projeto.create.module",
+        "projeto.retreave.module",
+        "projeto.update.module",
+        "projeto.delete.module",
+        "projeto.controller",
+        "projeto.routes",
+        'projeto.factory'
+    
+    ]);
---
+++ app/projeto/projeto.routes.js
@@ -0,0 +1,55 @@
+/**
+ * Created by Juanes on 21/05/2017.
+ */
+var projetoRoutes = angular.module('projeto.routes', ['ui.router']);
+
+projetoRoutes.config(function ($stateProvider) {
+
+    $stateProvider
+
+        .state('in.projeto.pesquisa',
+            {
+                parent: 'in.projeto',
+                url: "/pesquisar",
+                views: {
+                    "pesquisar": {
+                        templateUrl: "app/projeto/retreave/retreave.html",
+                        controller: 'projetoRetreaveCtrl'
+                    }
+                }
+            })
+        .state('in.projeto.cadastrar',
+            {
+                parent: 'in.projeto',
+                url: "/cadastrar",
+                views: {
+                    "cadastro": {
+                        templateUrl: "app/projeto/create/cadastro.html",
+                        controller: 'dashboardCtrl'
+                    }
+                }
+            })
+
+        .state('in.projeto.alterar',
+            {
+                parent: 'in.projeto',
+                url: '/alterar/{id}/',
+                controller: 'projetoUpdateCtrl',
+                views: {
+                    "pesquisar": {
+                        templateUrl: "app/projeto/retreave/retreave.html"
+                    }
+                }
+            })
+        .state('in.projeto.excluir',
+            {
+                parent: 'in.projeto',
+                url: '/excluir/{id}/',
+                controller: 'projetoDeleteCtrl',
+                views: {
+                    "pesquisar": {
+                        templateUrl: "app/projeto/retreave/retreave.html"
+                    }
+                }
+            })
+});
