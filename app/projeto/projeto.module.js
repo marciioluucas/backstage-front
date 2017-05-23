aedea1ff89a35857da33bd2f9fe9b8f453928fbc
marciioluucas <marciioluucas@gmail.com>
2017-05-23 13:34:42 -0300

    Alterando coisas e acrescentando outras

--- app/projeto/projeto.module.js
+++ app/projeto/projeto.module.js
@@ -1,6 +1,5 @@
 'use strict';
 var projetoModule = angular.module('projeto.module', [
-        "projeto.create.module",
         "projeto.retreave.module",
         "projeto.update.module",
         "projeto.delete.module",
