763eb0682eaf7acf08c9135fcd06f50ac36aedbb
marciioluucas <marciioluucas@gmail.com>
2017-05-31 00:23:09 -0300

    Colocando os arquivos de proposta

--- app/comum/comum.controller.js
+++ app/comum/comum.controller.js
@@ -2,5 +2,10 @@ var comumController = angular.module('comum.controller', []);
 
 comumController.controller('comumCtrl',
     ['$scope', function ($scope) {
-
+        $scope.fixarTop = function(){
+            var el = document.getElementsByName('body');
+            if(el.scrollTop > 50){
+                return true
+            }
+        }
     }]);
